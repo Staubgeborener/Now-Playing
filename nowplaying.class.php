449cdbaa8bc79702896381227fa9e5a7d0a34f1e
Staubgeborener <ericschroeder91@web.de>
2017-11-22 20:58:42 +0100

    Update nowplaying.class.php
    Add necessary variable for adding conditions 'now playing' and 'last heard'

--- nowplaying.class.php
+++ nowplaying.class.php
@@ -1,5 +1,4 @@
 <?php
-
 /**
 * Class that returns the track a user is currently playing via Last.fm
 *
@@ -7,48 +6,50 @@
 * @copyright    2011 Erik Jansson
 *
 */
-
 class NowPlaying{
-
     private $url;
     private $noTrackPlayingMessage;
-
+	
+	
     function __construct($user, $api_key){
-
         // construct URL
         $this->url  = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&limit=1';
         $this->url .= '&user=' . $user . '&api_key=' . $api_key;
-
         // default message
-        $this->noTrackPlayingMessage = 'Nothing is playing right now!';
+		//$this->noTrackPlayingMessage = 'Nothing is playing right now!';
     }
     
     // return the artist and track currently playing
     public function getNowPlaying(){
-
         // create an XML object
         $xml = simplexml_load_file($this->url);
-
         // get the latest track
         $track = $xml->recenttracks->track;
-
         // check if the track is actually playing 
         $nowplaying = $track->attributes()->nowplaying;
         
         // return the track and artist if music is playing, otherwise show message
+		
         if($nowplaying){
+			global $artist;
+			global $songname;
             $artist = $track->artist;
             $songname = $track->name;
+			//create file for last played song
+			file_put_contents('lastplayed.txt',$artist . "\n");
+			file_put_contents('lastplayed.txt',$songname, FILE_APPEND);
             return $artist . ' - ' . $songname;
         }
         else{
             return $this->noTrackPlayingMessage;
-        }
     }
+	}
     
     // set the message to be shown when no music is playing
     public function setNoTrackPlayingMessage($messageIn){
         $this->noTrackPlayingMessage = $messageIn;
     }
-
+	
+		
+	
 } // end class
